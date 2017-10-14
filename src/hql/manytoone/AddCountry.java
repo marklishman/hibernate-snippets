a2340d37e47fb1719c7eef726e432c2f23e6c6a8
marklishman <mark.lishman@gmail.com>
2017-10-14 14:22:59 +0100

    Many to One

--- src/hql/manytoone/AddCountry.java
+++ src/hql/manytoone/AddCountry.java
@@ -1,11 +1,11 @@
 session.beginTransaction();
 
-        Country serbia = new Country();
-        serbia.setName("Serbia");
-        serbia.setArea(34116);
+Country serbia = new Country();
+serbia.setName("Serbia");
+serbia.setArea(34116);
 
-        Continent europe = (Continent) session.load(Continent.class, 3);
-        europe.addCountry(serbia);
-        session.save(serbia);
+Continent europe = (Continent) session.load(Continent.class, 3);
+europe.addCountry(serbia);
+session.save(serbia);
 
-        session.getTransaction().commit();
+session.getTransaction().commit();
--- src/hql/manytoone/ContinentFromCountry.java
+++ src/hql/manytoone/ContinentFromCountry.java
@@ -1,3 +1,3 @@
 Country georgia = (Country) session.load(Country.class, 122);
-        System.out.println(georgia.getName() + " is in " +
-        georgia.getContinent().getName());
+System.out.println(georgia.getName() + " is in " +
+georgia.getContinent().getName());
